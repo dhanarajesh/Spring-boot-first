2634ac003dc986cc0f63809f3f2df166035c40b4
Rajesh k <Rajesh k@192.168.0.2>
2019-09-07 12:20:11 +0530

    Fist commit

---
+++ Myadvapp/src/main/java/com/main/MyadvappApplication.java
@@ -0,0 +1,15 @@
+package com.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyadvappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyadvappApplication.class, args);
+		
+		System.out.println("rajesh-app");
+	}
+
+}
